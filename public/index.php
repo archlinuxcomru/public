83cb4ec97c1a1d7c7b9819a89bad5082732a6810
indeviral <mb@ind.by>
2022-09-01 21:41:57 +0300

    изменено:      public/index.php

--- public/index.php
+++ public/index.php
@@ -9,10 +9,10 @@
       <p><h2>Добро пожаловать на сайт русскоязычного сообщества Arch Linux!</h2>Arch Linux™ - гибкий и простой дистрибутив, разработанный для удовлетворения нужд опытных пользователей Linux. Он является одновременно мощным и простым в управлении, что делает его идеальным дистрибутивом для серверов и рабочих станций.</p>
       <p>Развивайте систему на базе Arch в любом нужном направлении: если вы разделяете данный взгляд на то, каким должен быть дистрибутив GNU/Linux®, тогда добро пожаловать в сообщество Arch Linux!</p>
       <div align="right">
-      <a href="https://archlinux.com.ru/forum/viewtopic.php?p=145#p145" class="button" title="Кодекс поведения">
+      <a href="forum/viewtopic.php?p=145#p145" class="button" title="Кодекс поведения">
         <span>Кодекс поведения</span>
       </a>
-        <a href="https://archlinux.com.ru/forum/ucp.php?mode=login&redirect=ucp.php?mode=register" class="button" title="Присоединяйтесь!">
+        <a href="forum/ucp.php?mode=login&redirect=ucp.php?mode=register" class="button" title="Присоединяйтесь!">
         <span>Присоединяйтесь</span>
       </a>
       </div>
@@ -20,13 +20,13 @@
     <?php } ?>
     <div id="content-line">
       <div id="last-topic" class="main-box-left main-right">
-        <h2><a href="https://archlinux.com.ru/forum/search.php?search_id=active_topics">Последние изменения на форуме:</a></h2>
+        <h2><a href="forum/search.php?search_id=active_topics">Последние изменения на форуме:</a></h2>
         <ul>
         <?php main_get_rss("https://archlinux.com.ru/forum/feed/topics_active", "10"); ?>
         </ul>
       </div>
       <div id="news-topic" class="main-box-left">
-        <h2><a href="https://archlinux.com.ru/forum/viewforum.php?f=49">Локальные новости:</a></h2>
+        <h2><a href="forum/viewforum.php?f=49">Локальные новости:</a></h2>
         <ul>    
         <?php main_get_data_db('49','10'); ?>
         </ul>
@@ -34,13 +34,13 @@
     </div> <!-- END CONTENT LINE TWO -->
     <div id="content-line">
       <div id="last-blog" class="main-box-left main-right">
-        <h2><a href="https://archlinux.com.ru/forum/viewforum.php?f=50">Статьи и публикации:</a></h2>
+        <h2><a href="forum/viewforum.php?f=50">Статьи и публикации:</a></h2>
         <ul>
         <?php main_get_data_db('50','10'); ?>
         </ul>
       </div>
       <div id="gnews-topic" class="main-box-left">
-        <h2><a href="https://archlinux.com.ru/forum/viewforum.php?f=56">Глобальные новости:</a></h2>
+        <h2><a href="forum/viewforum.php?f=56">Глобальные новости:</a></h2>
         <ul>    
         <?php main_get_data_db('56','10'); ?>
         </ul>
@@ -77,7 +77,7 @@
       </ul>
       <h3>Сообщество:</h3>
       <ul>
-        <li><a href="https://archlinux.com.ru/forum/viewtopic.php?p=145#p145">Кодекс поведения</a></li>
+        <li><a href="forum/viewtopic.php?p=145#p145">Кодекс поведения</a></li>
       </ul>
     </div>
   </div><!--END CONTENT RIGHT -->
